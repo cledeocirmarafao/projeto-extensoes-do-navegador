07794e3ca9a073bef4f28c8091c070c789116e4c
Cledeocir Marafão <cledeocirms96@gmail.com>
2025-10-11 08:03:15 -0300

    new file: objeto screen.js componentizado

---
+++ src/javascript/objects/screen.js
@@ -0,0 +1,39 @@
+const screen = {
+  dataItens: document.querySelector(".container-cards"),
+  createCardHtml(cardData) {
+    return `
+            <div class="card">
+          <div class="extensions">
+            <img
+              src="${cardData.logo}"
+              alt="imagem de representação da extensão"
+            />
+            <div class="extensions-info">
+              <h2>${cardData.name}</h2>
+              <p>
+                ${cardData.description}
+              </p>
+            </div>
+          </div>
+          <div class="extensions-buttons">
+            <button class="remove" id="btn">Remove</button>
+
+            <label class="switch">
+              <input id="input" type="checkbox" />
+              <span class="slider"></span>
+            </label>
+          </div>
+        </div>
+        `;
+  },
+
+  renderScreen(info) {
+    const cardsHtmlArray = info.map((cardData) =>
+      this.createCardHtml(cardData)
+    );
+    const finalHtml = cardsHtmlArray.join("");
+    this.dataItens.innerHTML = finalHtml;
+  },
+};
+
+export { screen }
