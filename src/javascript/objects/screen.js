cd96a6ef1a528800a7e44b6eb091afd2bc3b9778
Cledeocir Marafão <cledeocirms96@gmail.com>
2025-10-12 04:46:51 -0300

    modified: criação de funções que salvam o estado ativo / inativo dos cards, linha 36 em diante

--- src/javascript/objects/screen.js
+++ src/javascript/objects/screen.js
@@ -2,7 +2,7 @@ const screen = {
   dataItens: document.querySelector(".container-cards"),
   createCardHtml(cardData) {
     return `
-            <div class="card">
+            <div class="card" data-card-id="${cardData.name}">
           <div class="extensions">
             <img
               src="${cardData.logo}"
@@ -19,7 +19,7 @@ const screen = {
             <button class="remove" id="btn">Remove</button>
 
             <label class="switch">
-              <input class="input" type="checkbox" />
+              <input class="input" type="checkbox" data-extension-name="${cardData.name}"/>
               <span class="slider"></span>
             </label>
           </div>
@@ -33,7 +33,77 @@ const screen = {
     );
     const finalHtml = cardsHtmlArray.join("");
     this.dataItens.innerHTML = finalHtml;
+
+    this.restoreCardsStates();
+    this.setupListeners();
   },
+
+  restoreCardsStates() {
+    const activesCards = JSON.parse(localStorage.getItem("activesCards")) || [];
+    activesCards.forEach((extensionName) => {
+      const switchInput = document.querySelector(
+        `input[data-extension-name="${extensionName}"]`
+      );
+      if (switchInput) {
+        switchInput.checked = true;
+
+        const card = document.querySelector(
+          `[data-card-id="${extensionName}"]`
+        );
+
+        if (card) {
+          this.moveToActive(card);
+        }
+      }
+    });
+  },
+
+  setupListeners() {
+    const allSwitches = document.querySelectorAll(
+      '.switch input[type="checkbox"]'
+    );
+
+    allSwitches.forEach((switchInput) => {
+      switchInput.addEventListener("change", (event) => {
+        const extensionName = event.target.dataset.extensionName;
+        const card = event.target.closest(".card");
+
+        if (event.target.checked) {
+          this.saveActiveCard(extensionName);
+          this.moveToActive(card);
+        } else {
+          this.removeActiveCard(extensionName);
+          this.moveToInactive(card);
+        }
+      });
+    });
+  },
+
+  saveActiveCard(extensionName) {
+    let activesCards = JSON.parse(localStorage.getItem('activesCards')) || []
+
+    if (!activesCards.includes(extensionName)) {
+      activesCards.push(extensionName)
+
+      localStorage.setItem('activesCards', JSON.stringify(activesCards))
+    }
+  },
+
+  removeActiveCards(extensionName) {
+    let activesCards = JSON.parse(localStorage.getItem('activesCards')) || []
+
+    activesCards = activesCards.filter(name => name !== extensionName)
+
+    localStorage.setItem('activesCards', JSON.stringify(activesCards))
+  },
+
+  moveToActive(card) {
+    card.classList.add('active')
+  },
+
+  moveToInactive(card) {
+    card.classList.remove('active')
+  }
 };
 
-export { screen }
+export { screen };
